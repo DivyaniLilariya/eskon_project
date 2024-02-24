672f3a6bb93bb4d47212ff2dc80f2cdb0489ba90
DivyaniLilariya <lilariyadivyani@gmail.com>
2024-02-24 20:40:53 +0530

    changes in pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
-import Header from "./header/page";
+
+import Link from 'next/link';
+import ThemeToggle from './ThemeToggle';
 
 
 const Home = () => {
   return (
  
-      <Header />
-    
+    <nav className="w-full py-5 dark:bg-white">
+    <div className="wrapper flex items-center justify-between">
+      <Link href={"/services"} className="flex-1">
+        <div className=" items-center">
+          <button className="  dark:text-dark">
+            Product
+          </button>
+          
+        </div>
+      </Link>
+      <Link href={"/details"}>
+        <div className=" items-center">
+          <button className="  dark:text-dark">
+            Details
+          </button>
+          
+        </div>
+      </Link>
+
+     
+      <div className="flex-1 justify-end flex gap-3 items-center">
+        <ThemeToggle />
+        
+      </div>
+    </div>
+  </nav>
   );
 };
 
