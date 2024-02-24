c0786bbbb31a785d6d0a22a74424ab23eb70bd34
DivyaniLilariya <lilariyadivyani@gmail.com>
2024-02-24 16:07:18 +0530

    changes in page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -16,7 +16,7 @@ export default async function Product() {
         <div className="flex flex-col items-center">
             <Header />
             <h1 className="mb-10">List of Products</h1>
-            {data?.map((product):any => (
+            {data?.map((product:any) => (
                 <Link key={product.sys.id} href="/details">
                     <div className="bg-red-500 p-10 mb-4 flex items-center justify-center cursor-pointer" style={{ height: "400px", width: "400px" }}>
                         <h2>{product?.fields?.heading}</h2>
