a3868ce9538b5b27288b6cffad51e0ae9286d4e4
DivyaniLilariya <lilariyadivyani@gmail.com>
2024-02-24 16:28:28 +0530

    add images

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,14 @@
 
-const images = [
-  
-  
-  'https://www.blackhillsinfosec.com/wp-content/uploads/2016/07/66619265.jpg',
-  'https://pbs.twimg.com/media/ER3cOugVAAIxavx?format=png&name=900x900',
-  'http://www.quickmeme.com/img/bd/bdb7ac37e00ff92776d0dead5171743db339c34a1f4f4c7293b3bde5ca960c79.jpg',
-  'http://www.quickmeme.com/img/eb/ebea0fc81dc1ac19db1eae42aee94d61dc09b934a53f94c7502f0ec4969457ed.jpg'
-
-];
-
-
 const getRandomImage = (): string => {
-  const randomIndex = Math.floor(Math.random() * images.length);
-  return images[randomIndex];
+  const imageFileNames = ['image1.avif', 'image2.jpg','image3.webp' ,'image4.jpg' ];
+  const randomImageFileName = imageFileNames[Math.floor(Math.random() * imageFileNames.length)];
+  const randomImageUrl = `/images/${randomImageFileName}`;
+  return randomImageUrl;
 };
 
+
 const RandomImage = () => {
-  
+
   const randomImageUrl = getRandomImage();
 
   return (
